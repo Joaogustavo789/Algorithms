100ecac209f0355a7a97020dc74443c5bca834a8
Joao Gustavo <jgustavomendonca@gmail.com>
2023-03-26 17:54:09 -0300

    Requisito 1: Estudantes estudando no mesmo horário

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,6 +1,17 @@
 def study_schedule(permanence_period, target_time):
     # print(permanence_period, target_time)
-    if permanence_period == "":
+    students_qt = 0
+
+    # print(target_time)
+    if target_time is None:
         return None
-    if target_time == "":
-        return None
+
+    for student in permanence_period:
+        if type(student[0]) is not int or type(student[1]) is not int:
+            return None
+
+        if target_time >= student[0] and target_time <= student[1]:
+            students_qt = students_qt + 1
+    # print(students_qt)
+
+    return students_qt
