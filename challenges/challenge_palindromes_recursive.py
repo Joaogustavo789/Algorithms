b7be1503dd6862d8a8b75d451b2022f808d69e3e
Joao Gustavo <jgustavomendonca@gmail.com>
2023-03-28 11:37:15 -0300

    Requisito 3: Palíndromo recursivo

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,13 +1,35 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    # print(word)
-    # print(low_index)
-    # print(high_index)
+    # print(word, low_index, high_index)
 
     if len(word) == 0:
         return False
 
-    elif len(word) == low_index and len(word) == high_index:
+    elif low_index >= high_index:
         return True
 
+    elif word[low_index] != word[high_index]:
+        return False
+
     else:
-        return is_palindrome_recursive(word, low_index, len(word) - 1)
+        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+
+# A R A R A
+# 0 1 2 3 4
+# i       j
+#   i   j
+#     ij
+# Aqui retorna True, pois i = A e j = A são iguais
+
+# O S S O
+# 0 1 2 3
+# i     j
+#   i j
+#   j i
+# Aqui retorna True, pois i é maior que j
+
+# A G U A
+# 0 1 2 3
+# i     j
+#   i j
+#   j i
+# Aqui retorna False, pois G e U são diferentes
