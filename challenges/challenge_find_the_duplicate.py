8b4937a3d1e23d0d5aedd26a79fdfc3ee6b931b5
Joao Gustavo <jgustavomendonca@gmail.com>
2023-03-30 12:21:43 -0300

    Requisito 5: Encontrar números repetidos

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -2,11 +2,13 @@ def find_duplicate(nums):
     if len(nums) <= 1:
         return False
 
-    # print(nums)
+    # print(nums)s
     for repeat in nums:
-        if not isinstance(repeat, int):
+        if not isinstance(repeat, int) or repeat < 0:
             return False
 
-        if repeat < 0:
-            return False
-    # print(repeat)!
+        if nums.count(repeat) > 1:
+            # print(repeat)
+            return repeat
+        # print(nums.count(repeat))
+    return False
