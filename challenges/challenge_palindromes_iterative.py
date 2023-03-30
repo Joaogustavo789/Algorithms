757c43e46c74666d9ba0e99ca93289d40aad73b8
Joao Gustavo <jgustavomendonca@gmail.com>
2023-03-30 00:05:16 -0300

    Requisito 6: Palíndromo iterativo

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,22 @@
 def is_palindrome_iterative(word):
     if len(word) == 0:
         return False
+
+    start_word = 0
+    end_word = len(word) - 1
+
+    while start_word < end_word:
+        if word[start_word] != word[end_word]:
+            return False
+
+        start_word = start_word + 1
+
+        end_word = end_word - 1
+
+    return True
+
+# A R A R A
+# s       e
+#  s    e
+#    se
+# Aqui "s" e "e" são iguais, com isso o laço while é encerrado.
