8d029f96f97e13771141de456261d9459b3842a8
Joao Gustavo <jgustavomendonca@gmail.com>
2023-03-30 18:18:17 -0300

    Corrigindo o tempo de execução

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -2,13 +2,28 @@ def find_duplicate(nums):
     if len(nums) <= 1:
         return False
 
-    # print(nums)s
-    for repeat in nums:
-        if not isinstance(repeat, int) or repeat < 0:
+    nums.sort()
+
+    for repeat in range(len(nums)):
+        if not isinstance(nums[repeat], int) or nums[repeat] < 0:
             return False
 
-        if nums.count(repeat) > 1:
-            # print(repeat)
-            return repeat
-        # print(nums.count(repeat))
+        decrement = repeat - 1
+
+        if nums[repeat] == nums[decrement]:
+            return nums[repeat]
+
     return False
+
+    # print(nums)
+    # for repeat in nums:
+    #     if not isinstance(repeat, int) or repeat < 0:
+    #         return False
+
+    #     # if nums.count(repeat) > 1:
+    #     #     # print(repeat)
+    #     #     return repeat
+
+    #     # return True
+
+    # return False
