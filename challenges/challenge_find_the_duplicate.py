c05c2e2676d1236fd1c8314df5a8330b1fb80cd7
Joao Gustavo <jgustavomendonca@gmail.com>
2023-03-30 11:33:27 -0300

    Nums recebendo string é igual a False

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -4,3 +4,9 @@ def find_duplicate(nums):
 
     if len(nums) == 1:
         return False
+
+    # print(nums)
+    for repeat in nums:
+        if not isinstance(repeat, int):
+            return False
+    # print(repeat)
