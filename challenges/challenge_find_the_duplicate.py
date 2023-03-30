deca3a4d0cde492061091eab12caa93e7ca40dc1
Joao Gustavo <jgustavomendonca@gmail.com>
2023-03-30 11:44:03 -0300

    Nums recebendo valores negativos é igual a False

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,12 +1,12 @@
 def find_duplicate(nums):
-    if len(nums) == 0:
-        return False
-
-    if len(nums) == 1:
+    if len(nums) <= 1:
         return False
 
     # print(nums)
     for repeat in nums:
         if not isinstance(repeat, int):
             return False
-    # print(repeat)
+
+        if repeat < 0:
+            return False
+    # print(repeat)!
